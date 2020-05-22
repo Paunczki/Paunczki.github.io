a87e194adebcf949850b450ffd9158703a787b23
Paunczki <30276245+Paunczki@users.noreply.github.com>
2020-05-22 11:56:39 -0500

    Updates

---
+++ precache-manifest.91ac8ac487d887e0d835917178ce156b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65fb59a9f5e3b2840e0a127305398cdf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4ff7e4c1a19b1e91a59",
+    "url": "/static/css/main.eace7cec.chunk.css"
+  },
+  {
+    "revision": "c623d30628c24fefe589",
+    "url": "/static/js/2.ca0397b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ca0397b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4ff7e4c1a19b1e91a59",
+    "url": "/static/js/main.895db8f8.chunk.js"
+  },
+  {
+    "revision": "9c72d3d3169ebbf433e6",
+    "url": "/static/js/runtime-main.dafc8f45.js"
+  }
+]);
