c5aae21513811ef6616d94009bc0e3454bce0897
Paunczki <30276245+Paunczki@users.noreply.github.com>
2020-05-23 15:20:50 -0500

    Work on multiple pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import profileImage from './img/download.jpg';
 import './App.css';
+import Home from './Home';
+import About from './About';
+import Projects from './Projects';
+import Contact from './Contact';
+import {Route, Link} from 'react-router-dom'
+import NavBar from './NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/about" component={About}/>
+      <Route exact path="/projects" component={Projects}/>
+      <Route exact path="/contact" component={Contact}/>
+      
       <h1>My React App</h1>
       <h2>Author: John Mikos</h2>
       <img src={profileImage} alt="profile-image" />
